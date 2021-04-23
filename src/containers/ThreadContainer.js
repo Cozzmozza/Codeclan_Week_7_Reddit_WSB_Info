10363731357b509f0afb3b0c8f74af83a8b1e4a3
Colin Morrison <colinjmmorris@gmail.com>
2021-04-23 14:46:11 +0100

    First Commit - Container Initialised

---
+++ src/containers/ThreadContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ThreadContainer = () => {
+
+
+    return(
+        <div className='Container'>
+            <h1>I am a container</h1>
+        </div>
+    )
+
+}
+
+export default ThreadContainer;
